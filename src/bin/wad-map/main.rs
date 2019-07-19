bfa559b38ef445439213c9de96acfb17565c2d85
Magnus Hovland Hoff <maghoff@gmail.com>
2019-07-19 15:18:35 +0200

    Split svg generation to separate module

--- src/bin/wad-map/main.rs
+++ src/bin/wad-map/main.rs
@@ -1,10 +1,11 @@
 extern crate wad_map;
 
-use std::io::Write;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
-use wad_map::*;
+use wad_map::{parse_linedefs, parse_vertexes};
+
+mod svg;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "wad-map", about = "Do stuff with map data in WAD files")]
@@ -34,36 +35,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let linedefs = map.by_id(b"LINEDEFS").ok_or("Cannot find LINEDEFS")?;
     let linedefs = parse_linedefs(&mut linedefs.clone())?;
 
-    let mut bbox = BoundingBox::from(&vertexes);
-    bbox.grow(20);
-
     let stdout = std::io::stdout();
-    let mut out = stdout.lock();
+    let out = stdout.lock();
 
-    writeln!(
-        out,
-        r#"<svg viewBox="{} {} {} {}" xmlns="http://www.w3.org/2000/svg">"#,
-        bbox.left(),
-        -bbox.bottom(),
-        bbox.width(),
-        bbox.height()
-    )?;
-    writeln!(out, "<style>{}</style>", include_str!("svg.css"))?;
-    for linedef in &linedefs {
-        let a = &vertexes[linedef.a as usize];
-        let b = &vertexes[linedef.b as usize];
-
-        let portal = linedef.left_sidedef.is_some() && linedef.right_sidedef.is_some();
-
-        let class = if portal { r#" class="portal""# } else { "" };
-
-        writeln!(
-            out,
-            r#"<line x1="{}" y1="{}" x2="{}" y2="{}"{} />"#,
-            a.x, -a.y, b.x, -b.y, class
-        )?;
-    }
-    writeln!(out, r#"</svg>"#)?;
+    svg::generate_svg(out, &vertexes, &linedefs)?;
 
     Ok(())
 }
---
+++ src/bin/wad-map/svg.rs
@@ -0,0 +1,36 @@
+use std::io::Write;
+
+use wad_map::{Vertex, Linedef, BoundingBox};
+
+pub fn generate_svg(mut out: impl Write, vertexes: &[Vertex], linedefs: &[Linedef]) -> std::io::Result<()> {
+    let mut bbox = BoundingBox::from(vertexes);
+    bbox.grow(20);
+
+    writeln!(
+        out,
+        r#"<svg viewBox="{} {} {} {}" xmlns="http://www.w3.org/2000/svg">"#,
+        bbox.left(),
+        -bbox.bottom(),
+        bbox.width(),
+        bbox.height()
+    )?;
+    writeln!(out, "<style>{}</style>", include_str!("svg.css"))?;
+    for linedef in linedefs {
+        let a = &vertexes[linedef.a as usize];
+        let b = &vertexes[linedef.b as usize];
+
+        let portal = linedef.left_sidedef.is_some() && linedef.right_sidedef.is_some();
+
+        let class = if portal { r#" class="portal""# } else { "" };
+
+        writeln!(
+            out,
+            r#"<line x1="{}" y1="{}" x2="{}" y2="{}"{} />"#,
+            a.x, -a.y, b.x, -b.y, class
+        )?;
+    }
+    writeln!(out, r#"</svg>"#)?;
+
+    Ok(())
+}
+
